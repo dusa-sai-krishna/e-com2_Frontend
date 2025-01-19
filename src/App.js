78d4ab47790ce8a20d1115999e65d71903349024
dusa_sai_krishna <dsai9441695421@gmail.com>
2025-01-19 22:38:58 +0530

    Redux  installed2

--- src/App.js
+++ src/App.js
@@ -8,19 +8,18 @@ import Product from "./customer/components/Product/Product";
 import ProductDetail from "./customer/components/Product/ProductDetail";
 import Cart from "./customer/components/Cart/Cart";
 import Checkout from "./customer/components/Checkout/Checkout";
+import Order from "./customer/components/Order/Order";
+import OrderDetail from "./customer/components/Order/OrderDetail";
+import {Route, Routes} from "react-router-dom";
+import CustomerRouter from "./Routers/CustomerRouter";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-        <div>
-          {/*<HomePage />*/}
-          {/*<Product/>*/}
-          {/*<ProductDetail/>*/}
-          {/*<Cart/>*/}
-          <Checkout/>
-          <Footer />
-        </div>
+
+      <Routes>
+        <Route path="/*" element={<CustomerRouter/>}></Route>
+      </Routes>
     </div>
   );
 }
